8d55702b2513ae86272093b4e41638280defc999
Otto Frankel <ottofrankel@comcast.net>
2021-08-23 11:30:58 -0700

    Updates state with fetched game

--- src/reducers/games-reducer.js
+++ src/reducers/games-reducer.js
@@ -1,12 +1,19 @@
 import { FETCH_GAME } from '../constants';
-import { xml2json } from 'xml-js';
+import { xml2js } from 'xml-js';
 
 const boardGamesReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_GAME:
-      const jsonData = xml2json(action.payload.data, {compact: false, spaces: 4});
+      const jsonData = xml2js(action.payload.data, {compact: true, spaces: 4});
+
       console.log(jsonData);
-      return state;
+
+      const game = {
+        id: jsonData.items.item._attributes.id,
+        name: jsonData.items.item.name._attributes.value
+      }
+
+      return [...state, game];
     default:
         return state;
   }
