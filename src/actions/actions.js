2e4422b0e9353d4b79d729462195ebc27b7ff95b
Otto Frankel <ottofrankel@comcast.net>
2021-08-24 11:56:04 -0700

    Added action creator to fetch multiple game ids

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -4,52 +4,67 @@ import { xml2js } from 'xml-js';
 import { ROOT_URL, ID_FETCH_URL, FETCH_MULTIPLE_URL, FETCH_GAME_BY_ID_SUCCESS } from '../constants';
 
 // Fetches a game based on a search query and dispatches fetchGameById with the result's id attribute
-export function fetchGame(query) {
-  return (dispatch) => {
-    axios.get(`${ROOT_URL}${query}`)
-    .then(response => {
-      const data = xml2js(response.data, {compact: true, space: 4});
-      const item = data.items.item;
+// export function fetchGame(query) {
+//   return (dispatch) => {
+//     axios.get(`${ROOT_URL}${query}`)
+//     .then(response => {
+//       const data = xml2js(response.data, {compact: true, space: 4});
+//       const item = data.items.item;
 
-      // If more than one item gets fetched, use the first one
-      if (isArray(item))
-        dispatch(fetchGameById(item[0]._attributes.id))
-      else
-        dispatch(fetchGameById(item._attributes.id));
-    })
+//       // If more than one item gets fetched, use the first one
+//       if (isArray(item))
+//         dispatch(fetchGameById(item[0]._attributes.id))
+//       else
+//         dispatch(fetchGameById(item._attributes.id));
+//     })
 
-    // We can dispatch error handling at this stage
-    .catch(error => {
-      console.log(error);
-    }) 
-  }
-}
+//     // We can dispatch error handling at this stage
+//     .catch(error => {
+//       console.log(error);
+//     }) 
+//   }
+// }
 
 export function fetchGames(query) {
   return (dispatch) => {
     axios.get(`${FETCH_MULTIPLE_URL}${query}`)
     .then(response => {
-      const data = xml2js(response.data, {compact: true, space: 4});
-      console.log(data.items.item);
-    })
 
-    // We can dispatch error handling at this stage
+      const data = xml2js(response.data, {compact: true, space: 4});
+
+      const ids = data.items.item.map((game) => {
+        return game._attributes.id
+      })
+      
+      dispatch(fetchGamesByIds(ids));
+    })
     .catch(error => {
       console.log(error);
-    }) 
+    })
   }
 }
 
 // Fetches a game based off of a supplied id and dispatches fetchGameIdSuccess with the result
-export function fetchGameById(gameId) {
+// export function fetchGameById(gameId) {
+//   return (dispatch) => {
+//     axios.get(`${ID_FETCH_URL}${gameId}`)
+//     .then(response => {
+//       const game = xml2js(response.data, {compact: true, space: 4});
+//       dispatch(fetchGameByIdSuccess(game));
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     })
+//   }
+// }
+
+export function fetchGamesByIds(ids) {
   return (dispatch) => {
-    axios.get(`${ID_FETCH_URL}${gameId}`)
+    axios.get(`${ID_FETCH_URL}${ids.join()}`)
     .then(response => {
-      const game = xml2js(response.data, {compact: true, space: 4});
-      dispatch(fetchGameByIdSuccess(game));
-    })
-    .catch(error => {
-      console.log(error);
+      
+      const data = xml2js(response.data, {compact: true, space: 4});
+      console.log(data);
     })
   }
 }
