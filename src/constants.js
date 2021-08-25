8e1b38ac9d582f43946a58e86eba19828b5eca58
Otto Frankel <ottofrankel@comcast.net>
2021-08-25 12:43:22 -0700

    constants changed

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const ADD_GAME = "ADD_GAME";
 export const FETCH_GAMES_ERROR = "FETCH_GAMES_ERROR";
 export const CLEAR_RESULTS = "CLEAR_RESULTS";
 export const CLEAR_ERROR = "CLEAR_ERROR";
-export const ID_FETCH_URL = "https://www.boardgamegeek.com/xmlapi2/thing?id=";
+export const ID_FETCH_URL = "https://www.boardgamegeek.com/xmlapi2/thing?&stats=1&id=";
 export const BACK_ARROW_IMG = "https://image.flaticon.com/icons/png/512/60/60577.png";
 export const REMOVE_GAME_IMG = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHvQzPD7lYrTQvIywYogS50koNq25j2A3K7Q&usqp=CAU";
 export const MAG_GLASS_IMG = "https://static.thenounproject.com/png/340642-200.png";
