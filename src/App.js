8c0bb1c590ad1e2ec09e514ae0c40d2214f54f10
Ryan Passer <rpasser93@gmail.com>
2021-08-25 04:00:18 -0400

    Update subtitle on list page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div>
       <div className ="text-center">
         <h1 className="title">Board Game Upgrader</h1>
-        <h5>Build out your game shelf so we can build it further:</h5>
+        <h5>Build out your game shelf. We'll help you take it up a notch:</h5>
       </div>
       <SearchBar />
       <BoardGameList/>
