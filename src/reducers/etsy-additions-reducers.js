de33db6edb1166370198b31636a33bb6110e88bc
Otto Frankel <ottofrankel@comcast.net>
2021-08-26 15:24:05 -0700

    Reducer returns array of additions

--- src/reducers/etsy-additions-reducers.js
+++ src/reducers/etsy-additions-reducers.js
@@ -3,8 +3,14 @@ import { FETCH_ETSY_ADDITIONS_SUCCESS } from "../constants";
 const etsyAdditionsReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_ETSY_ADDITIONS_SUCCESS:
-      console.log(action.payload);
-      return state;
+      const additions = action.payload.data.results.map((addition) => {
+        return {
+          id: addition.listing_id,
+          title: addition.title,
+          imgUrl: addition.MainImage.url_170x135
+        }
+      })
+      return additions;
     default:
       return state;
   }
